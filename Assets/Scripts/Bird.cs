b5fba5608e016a9308151d8f8c5235c706c0b01f
Dakota Robbins <drobbinso@gmail.com>
2018-04-21 20:17:31 -0600

    Bird can fly.

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    private Rigidbody _rigidbody;
+
+    public float Force;
+
+
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Debug.Log("I Jumped!");
+            //_rigidbody.AddForce(new Vector3(0, 5, 0), ForceMode.VelocityChange);
+            _rigidbody.velocity = new Vector3(0, Force, 0);
+        }
+	}
+}
