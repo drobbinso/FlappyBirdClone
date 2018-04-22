47bf20628e0960b472ecc2dcf60e7f9fb73d1c8b
Dakota Robbins <drobbinso@gmail.com>
2018-04-21 20:40:20 -0600

    Pipe moves!

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+    public float MoveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(MoveSpeed*Time.deltaTime, 0, 0));
+	}
+}
